7cf6d3ab10b8ae7cc61a7a9dc884cdc0b928fd8b
瑞宇 <157418979@qq.com>
2022-04-26 18:43:24 +0800

    添加cancle

--- lib/flutter_universalad_stream.dart
+++ lib/flutter_universalad_stream.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: cancel_subscriptions
+
 import 'dart:async';
 import 'dart:io';
 
@@ -12,101 +14,105 @@ import 'method_callback.dart';
 /// @Author: gstory
 /// @CreateDate: 2021/8/10 7:08 下午
 /// @Description: dart类作用描述
+class FlutterUniversalAdStreamSubscription {
+  StreamSubscription _tencentSubscription;
+  StreamSubscription _pangleSubscription;
+
+  FlutterUniversalAdStreamSubscription(
+      this._tencentSubscription, this._pangleSubscription);
+
+  void cancel() {
+    _tencentSubscription.cancel();
+    _pangleSubscription.cancel();
+  }
+}
 
 class FlutterUniversalAdStream {
-  static void initAdStream({
+  static FlutterUniversalAdStreamSubscription initAdStream({
     URewardCallBack? uRewardCallBack,
     UInteractionCallBack? uInteractionCallBack,
   }) {
-    FlutterTencentAdStream.initAdStream(
+    StreamSubscription _tencentSubscription =
+        FlutterTencentAdStream.initAdStream(
       //激励广告
       flutterTencentadRewardCallBack: FlutterTencentadRewardCallBack(
-        onShow: () {
-          // print("激励广告显示");
-          uRewardCallBack?.onShow!(UniversalSdkKType.TENCENT);
-        },
-        onClick: () {
-          // print("激励广告点击");
-          uRewardCallBack?.onClick!(UniversalSdkKType.TENCENT);
-        },
-        onFail: (code, message) {
-          // print("激励广告失败 $code $message");
-          if (AdManage.instance.rewardLoadBean.lastShowSuccess) {
-            AdManage.instance.rewardLoadBean.lastShowSuccess = false;
-            AdManage.instance.loadRewardAd(UniversalSdkKType.PANGOLIN);
-          } else {
-            uRewardCallBack?.onFail!(UniversalSdkKType.TENCENT, code, message);
-          }
-        },
-        onClose: () {
-          // print("激励广告关闭");
-          uRewardCallBack?.onClick!(UniversalSdkKType.TENCENT);
-        },
-        onReady: () async {
-          AdManage.instance.rewardLoadBean.lastSdk = UniversalSdkKType.TENCENT;
-          // print("激励广告预加载准备就绪");
-          uRewardCallBack?.onReady!(UniversalSdkKType.TENCENT);
-        },
-        onUnReady: () {
-          // print("激励广告预加载未准备就绪");
-          uRewardCallBack?.onUnReady!(UniversalSdkKType.TENCENT);
-        },
-        onVerify: (transId,rewardName,rewardAmount) {
-          // print("激励广告奖励  $transId");
-          uRewardCallBack?.onVerify!(
-              UniversalSdkKType.TENCENT,
-              transId,
-              true,
-              rewardAmount,
-              rewardName);
-        },
-        onFinish: (){
-
-        },
-        onExpose: (){
-
-        }
-      ),
+          onShow: () {
+            // print("激励广告显示");
+            uRewardCallBack?.onShow!(UniversalSdkKType.TENCENT);
+          },
+          onClick: () {
+            // print("激励广告点击");
+            uRewardCallBack?.onClick!(UniversalSdkKType.TENCENT);
+          },
+          onFail: (code, message) {
+            // print("激励广告失败 $code $message");
+            if (AdManage.instance.rewardLoadBean.lastShowSuccess) {
+              AdManage.instance.rewardLoadBean.lastShowSuccess = false;
+              AdManage.instance.loadRewardAd(UniversalSdkKType.PANGOLIN);
+            } else {
+              uRewardCallBack?.onFail!(
+                  UniversalSdkKType.TENCENT, code, message);
+            }
+          },
+          onClose: () {
+            // print("激励广告关闭");
+            uRewardCallBack?.onClick!(UniversalSdkKType.TENCENT);
+          },
+          onReady: () async {
+            AdManage.instance.rewardLoadBean.lastSdk =
+                UniversalSdkKType.TENCENT;
+            // print("激励广告预加载准备就绪");
+            uRewardCallBack?.onReady!(UniversalSdkKType.TENCENT);
+          },
+          onUnReady: () {
+            // print("激励广告预加载未准备就绪");
+            uRewardCallBack?.onUnReady!(UniversalSdkKType.TENCENT);
+          },
+          onVerify: (transId, rewardName, rewardAmount) {
+            // print("激励广告奖励  $transId");
+            uRewardCallBack?.onVerify!(UniversalSdkKType.TENCENT, transId, true,
+                rewardAmount, rewardName);
+          },
+          onFinish: () {},
+          onExpose: () {}),
       //插屏广告
       flutterTencentadInteractionCallBack: FlutterTencentadInteractionCallBack(
-        onShow: () {
-          // print("插屏广告显示");
-          uInteractionCallBack?.onShow!(UniversalSdkKType.TENCENT);
-        },
-        onClick: () {
-          // print("插屏广告点击");
-          uInteractionCallBack?.onClick!(UniversalSdkKType.TENCENT);
-        },
-        onFail: (code, message) {
-          // print("插屏广告失败 $code $message");
-          if (AdManage.instance.interstitialLoadBean.lastShowSuccess) {
-            AdManage.instance.interstitialLoadBean.lastShowSuccess = false;
-            AdManage.instance.loadInteractionAd(UniversalSdkKType.PANGOLIN);
-          } else {
-            uInteractionCallBack?.onFail!(
-                UniversalSdkKType.TENCENT, code, message);
-          }
-        },
-        onClose: () {
-          // print("插屏广告关闭");
-          uInteractionCallBack?.onClose!(UniversalSdkKType.TENCENT);
-        },
-        onReady: () async {
-          AdManage.instance.interstitialLoadBean.lastSdk = UniversalSdkKType.TENCENT;
-          // print("插屏广告预加载准备就绪");
-          uInteractionCallBack?.onReady!(UniversalSdkKType.TENCENT);
-        },
-        onUnReady: () {
-          print("2插屏广告预加载未准备就绪");
-          uInteractionCallBack?.onUnReady!(UniversalSdkKType.TENCENT);
-        },
-        onExpose: (){
-
-        }
-      ),
+          onShow: () {
+            // print("插屏广告显示");
+            uInteractionCallBack?.onShow!(UniversalSdkKType.TENCENT);
+          },
+          onClick: () {
+            // print("插屏广告点击");
+            uInteractionCallBack?.onClick!(UniversalSdkKType.TENCENT);
+          },
+          onFail: (code, message) {
+            // print("插屏广告失败 $code $message");
+            if (AdManage.instance.interstitialLoadBean.lastShowSuccess) {
+              AdManage.instance.interstitialLoadBean.lastShowSuccess = false;
+              AdManage.instance.loadInteractionAd(UniversalSdkKType.PANGOLIN);
+            } else {
+              uInteractionCallBack?.onFail!(
+                  UniversalSdkKType.TENCENT, code, message);
+            }
+          },
+          onClose: () {
+            // print("插屏广告关闭");
+            uInteractionCallBack?.onClose!(UniversalSdkKType.TENCENT);
+          },
+          onReady: () async {
+            AdManage.instance.interstitialLoadBean.lastSdk =
+                UniversalSdkKType.TENCENT;
+            // print("插屏广告预加载准备就绪");
+            uInteractionCallBack?.onReady!(UniversalSdkKType.TENCENT);
+          },
+          onUnReady: () {
+            print("2插屏广告预加载未准备就绪");
+            uInteractionCallBack?.onUnReady!(UniversalSdkKType.TENCENT);
+          },
+          onExpose: () {}),
     );
 
-    FlutterUnionadStream.initAdStream(
+    StreamSubscription _pangleSubscription = FlutterUnionadStream.initAdStream(
       //激励广告
       flutterUnionadRewardAdCallBack: FlutterUnionadRewardAdCallBack(
         onShow: () {
@@ -143,7 +149,7 @@ class FlutterUniversalAdStream {
           // print("激励广告预加载未准备就绪");
           uRewardCallBack?.onUnReady!(UniversalSdkKType.PANGOLIN);
         },
-        onVerify: (rewardVerify, rewardAmount, rewardName,errorCode,error) {
+        onVerify: (rewardVerify, rewardAmount, rewardName, errorCode, error) {
           // print("激励广告奖励  $rewardVerify   $rewardAmount  $rewardName");
           uRewardCallBack?.onVerify!(UniversalSdkKType.PANGOLIN, "",
               rewardVerify, rewardAmount, rewardName);
@@ -180,7 +186,8 @@ class FlutterUniversalAdStream {
           uInteractionCallBack?.onClose!(UniversalSdkKType.PANGOLIN);
         },
         onReady: () async {
-          AdManage.instance.interstitialLoadBean.lastSdk = UniversalSdkKType.PANGOLIN;
+          AdManage.instance.interstitialLoadBean.lastSdk =
+              UniversalSdkKType.PANGOLIN;
           // print("新模板渲染插屏广告预加载准备就绪");
           uInteractionCallBack?.onReady!(UniversalSdkKType.PANGOLIN);
         },
@@ -190,5 +197,7 @@ class FlutterUniversalAdStream {
         },
       ),
     );
+    return FlutterUniversalAdStreamSubscription(
+        _tencentSubscription, _pangleSubscription);
   }
 }
